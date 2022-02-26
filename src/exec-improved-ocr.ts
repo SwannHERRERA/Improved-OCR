5edbd05326a61a4aefa66b95eae0f016e141674c
RemyMach <remygt@hotmail.fr>
2022-02-26 17:07:30 +0100

    add start of cli tool

---
+++ src/exec-improved-ocr.ts
@@ -0,0 +1,19 @@
+import { argv } from 'process';
+import { Cli } from './cli';
+import { Command } from './command';
+import { argsConfigured, DIGIT_HEIGHT, DIGIT_WIDTH } from './config';
+import { Parser } from './parser';
+
+
+function main(): void {
+    
+    const input = argv.splice(2).join(' ')
+    const cli = new Cli(new Map(), argsConfigured);
+    const parser = new Parser(DIGIT_WIDTH, DIGIT_HEIGHT);
+    cli.parse(input);
+    const map = cli.getArgsParsed();
+    const commandClass = new Command(parser, argsConfigured, map);
+    commandClass.process();
+}
+
+main();
