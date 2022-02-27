01f7df684d367680b6f5e9a66c053f89d43d9fc9
Swann HERRERA <swann@devloup.dev>
2022-02-27 10:18:50 +0100

    rename command

--- src/command-interactor.ts
+++ src/command-interactor.ts
@@ -3,33 +3,33 @@ import { parse, Parser } from './parser';
 import { Classify, ClassifyGroup, ClassifySingle, codeToResultFormat } from './write-code-result';
 import { WriterInConsole } from './writer/writer-in-console';
 
-export class Command {
+export class CommandInteractor {
     private parser: Parser;
     private argsConfigured: Map<CliFunctionnality, string>;
-    private argsParsed: Map<string, string[]>;
 
-    constructor(
-        parser: Parser,
-        argsConfigured: Map<CliFunctionnality, string>,
-        argsParsed: Map<string, string[]>
-    ) {
+    constructor(parser: Parser, argsConfigured: Map<CliFunctionnality, string>) {
         this.parser = parser;
         this.argsConfigured = argsConfigured;
-        this.argsParsed = argsParsed;
     }
 
-    public async process() {
-        const inputFiles = this.argsParsed.get(
+    public async meshToOutput(argsParsed: Map<string, string[]>) {
+        const inputFiles = argsParsed.get(
             this.argsConfigured.get(CliFunctionnality.INPUT_FILE) ?? ''
         );
-        const outputFiles = this.argsParsed.get(
+        const outputFiles = argsParsed.get(
             this.argsConfigured.get(CliFunctionnality.OUTPUT_FILE) ?? ''
         );
-        const isConsoleOutput = this.argsParsed.has(
+        const isConsoleOutput = argsParsed.has(
             this.argsConfigured.get(CliFunctionnality.CONSOLE_OUTPUT) ?? ''
         );
 
         if (inputFiles) {
+            // TODO
+            // Faire ce traitement de manière groupé
+            // const content = await parse(path);
+            // const codes = this.parser.extractCodes(content);
+            // const output = codes.map((code) => codeToResultFormat(code));
+
             if (isConsoleOutput) {
                 const Writer = new WriterInConsole();
                 for (const path of inputFiles) {
--- src/exec-improved-ocr.ts
+++ src/exec-improved-ocr.ts
@@ -1,6 +1,6 @@
 import { argv } from 'process';
 import { Cli } from './cli';
-import { Command } from './command';
+import { CommandInteractor } from './command-interactor';
 import { argsConfigured, DIGIT_HEIGHT, DIGIT_WIDTH } from './config';
 import { Parser } from './parser';
 
@@ -9,9 +9,9 @@ function main(): void {
     const cli = new Cli(new Map(), argsConfigured);
     const parser = new Parser(DIGIT_WIDTH, DIGIT_HEIGHT);
     cli.parse(input);
-    const map = cli.getArgsParsed();
-    const commandClass = new Command(parser, argsConfigured, map);
-    commandClass.process();
+    const argument = cli.getArgsParsed();
+    const commandInteractor = new CommandInteractor(parser, argsConfigured);
+    commandInteractor.meshToOutput(argument);
 }
 
 main();
